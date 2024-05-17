71e57ff0442b08178eb7100a475e16cfc60b4335
Steve Brownlee <chortlehoort@gmail.com>
2024-05-17 13:29:50 -0500

    Double quotes??? Why?

--- generate.py
+++ generate.py
@@ -1,19 +1,20 @@
 import random
 import string
 
+
 def validate_password(password, char_type_dict):
     is_valid = True
 
-    if 'lower' in char_type_dict and not any(ch.islower() for ch in password):
+    if "lower" in char_type_dict and not any(ch.islower() for ch in password):
         is_valid = False
 
-    if 'upper' in char_type_dict and not any(ch.isupper() for ch in password):
+    if "upper" in char_type_dict and not any(ch.isupper() for ch in password):
         is_valid = False
 
-    if 'digit' in char_type_dict and not any(ch.isdigit() for ch in password):
+    if "digit" in char_type_dict and not any(ch.isdigit() for ch in password):
         is_valid = False
 
-    if 'special' in char_type_dict and not any(not ch.isalnum() for ch in password):
+    if "special" in char_type_dict and not any(not ch.isalnum() for ch in password):
         is_valid = False
 
     return is_valid
@@ -40,7 +41,9 @@ def generate_password(length, char_type_dict):
         # Repeat the process until the desired password length is reached
 
         # Randomly select a character type
-        random_type = random.choice(list(char_type_dict.keys())) # this will be 'lower', 'digit', etc
+        random_type = random.choice(
+            list(char_type_dict.keys())
+        )  # this will be 'lower', 'digit', etc
 
         # Randomly select a character from that type
         key_value_we_need = char_type_dict[random_type]
@@ -51,6 +54,7 @@ def generate_password(length, char_type_dict):
 
     # Return the generated password
     return password_string
+
 
 def evaluate_password_strength(password):
     """Evaluate the strength of the provided password
@@ -78,28 +82,28 @@ def evaluate_password_strength(password):
     if any(not ch.isalnum() for ch in password):
         score += 10
 
-
     # Determine the password strength category based on the strength score
     # Less than 50 is Weak, 50-79 is Moderate, 80-99 is Strong, 100 or more is Very Strong
     category = ""
     if score < 50:
         category = "Weak"
-    elif score >= 50 and score <=79:
+    elif score >= 50 and score <= 79:
         category = "Moderate"
-    elif score >= 80 and score <=99:
+    elif score >= 80 and score <= 99:
         category = "Strong"
     else:
         category = "Very Strong"
 
     # Return a tuple containing the strength score and strength category
-    return ( score, category )
+    return (score, category)
+
 
 # Prompt the user for the desired password length
 # password_length = int(input("Enter the desired password length: "))
 password_length = 8
 
 # Prompt the user to select character types (lowercase, uppercase, digits, special characters)
-char_types = ['lower', 'digit', 'special', 'upper']
+char_types = ["lower", "digit", "special", "upper"]
 # if input("Include lowercase letters? (y/n): ").lower() == 'y':
 #     char_types.append('lower')
 
@@ -115,10 +119,10 @@ char_types = ['lower', 'digit', 'special', 'upper']
 
 # Create a dictionary mapping character types to their corresponding character sets
 char_type_dict = {
-    'lower': list(string.ascii_lowercase),
-    'upper': list(string.ascii_uppercase),
-    'digit': list(string.digits),
-    'special': list(string.punctuation)
+    "lower": list(string.ascii_lowercase),
+    "upper": list(string.ascii_uppercase),
+    "digit": list(string.digits),
+    "special": list(string.punctuation),
 }
 
 # Filter the character type dictionary based on user-selected types. Generate a new dictionary with only the selected types.
@@ -141,17 +145,8 @@ while len(unique_passwords) < 5:
     unique_passwords.add(final_password)
 
     score, category = evaluate_password_strength(final_password)
-    print(f'Score of password is {score} and its category is {category}')
+    print(
+        f"Score of password {final_password} is {score} and its category is {category}"
+    )
 
 print(unique_passwords)
-
-# Display each generated password along with its strength score and category
-# Example:
-#    Generated password: d6Gh%Fk8Lm1Q
-#    Password strength score: 88
-#    Password strength category: Strong
-
-
-# Print all the generated passwords stored in the set
-
-
